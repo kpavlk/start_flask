b3027bdcdb89824c1ce1fb4eb69e279bbc032455
Kostya <pavlenkkovkosta@gmail.com>
2024-04-29 10:17:45 +0300

    Results

--- mars_picture.py
+++ mars_picture.py
@@ -209,6 +209,34 @@ def choice_planet(planet):
                 </html>"""
 
 
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                   <link rel="stylesheet" href="static/css/style.css">
+                    <title>Результаты</title>
+                  </head>
+                  <body>
+                    <h2>Результаты отбора</h2>
+                    <h3>Претендента на участие в миссии: {nickname}</h3>
+                    <div class="alert alert-success" role="alert">
+                      <h4>Поздравляем! Ваш рейтинг после {level} этапа отбора</h4>
+                    </div>
+                      <h3>составляет {rating}!</h3>
+                    <div class="alert alert-warning" role="alert">
+                      <h3>Желаем удачи!</h3>
+                    </div>
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
 
